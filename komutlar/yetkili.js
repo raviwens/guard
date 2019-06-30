c609f0f8b909127b16ae4d158277af16bc5cf8b3
Glitch (hello-express) <none>
2019-06-30 20:54:31 +0000

    🏩💡 Checkpoint ./komutlar/yardım.js:4799852/6430 ./komutlar/yetkili.js:4799852/1035 ./komutlar/eğlence.js:4799852/1035 ./komutlar/kullanıcı.js:4799852/1041 ./watch.json:4799852/2

---
+++ komutlar/yetkili.js
@@ -0,0 +1,27 @@
+const Discord = require('discord.js');
+
+let botid = ('') //bu yere botun id'sini yapıştırın.
+//eğer botunuz dbl(discord bot list) de yoksa Bota Oy Ver (Vote) olmucaktır.
+
+exports.run = (client, message, args) => {
+    const embed = new Discord.RichEmbed()
+    .setColor("RANDOM")
+    .setAuthor(`${client.user.username} Yetkili Komutları`)
+    .addField('prefix+komut', 'açıklama/kullanım amacı')//ne kadar yetkili komutunuz varsa o kadar .addField('prefix+komut', 'açıklama/kullanım amacı') koyun
+    .addField(`» Linkler`, `[Bot Davet Linki](https://discordapp.com/oauth2/authorize?client_id=${botid}&scope=bot&permissions=8) **|** [Destek Sunucusu](https://discord.gg/BAĞLANTI) **|** [Bota Oy Ver (Vote)](https://discordbots.org/bot/${botid}/vote) **|** [Web Sitesi]()`)//websiteniz yoksa  **|** [Web Sitesi]() yeri silebilirsiniz
+    message.channel.sendEmbed(embed);
+
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0,
+};
+
+exports.help = {
+  name: 'yetkili',
+  description: '',
+  usage: ''
+};
