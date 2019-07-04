9bcf0f52ccfa9196a7d90d7e36c9b5c19219d842
Glitch (hello-express) <none>
2019-07-04 17:47:03 +0000

    👐👠 Checkpoint ./komutlar/elraenn.js:4799852/1 ./watch.json:4799852/2

--- komutlar/elraenn.js
+++ komutlar/elraenn.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-    let replies = ["https://i.hizliresim.com/NLgD7L.jpg"];
+    let replies = ["https://i.hizliresim.com/NLgD7L.jpg",];
 
     let result = Math.floor((Math.random() * replies.length));
 
