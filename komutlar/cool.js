af01810206ad885d85231cd2f3d412400176f495
Glitch (hello-express) <none>
2019-07-05 18:35:14 +0000

    🍴🏔 Checkpoint ./komutlar/cool.js:4799852/604 ./komutlar/elraenn.js:4799852/406 ./komutlar/wtcN.js:4799852/565 ./komutlar/rammus53.js:4799852/572 ./watch.json:4799852/6

--- komutlar/cool.js
+++ komutlar/cool.js
@@ -1,6 +1,10 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
+  
+
+  
+  
 
     let replies = ["https://i.postimg.cc/QdrCtW6N/12421.jpg","https://i.postimg.cc/s2TgwH31/13421.jpg,","https://i.postimg.cc/t4tTVCf3/21312.jpg","https://i.postimg.cc/KYGY7hSd/21321.jpg","https://i.postimg.cc/zB3XMwf7/21421.jpg","https://i.postimg.cc/bJFdNC54/21sa.jpg","https://i.postimg.cc/j2w2W602/21sd.jpg","https://i.postimg.cc/Y059Q4M3/21sfa.jpg","https://i.postimg.cc/fWpkGPm9/231.jpg","https://i.postimg.cc/ZKN04LxD/2431.jpg","https://i.postimg.cc/FRqsQfd4/ascas.jpg","https://i.postimg.cc/z3YzV9F2/ascsa.jpg","https://i.postimg.cc/gjZccY86/asdas.jpg","https://i.postimg.cc/bYD1SkR1/asdaso-j.jpg","https://i.postimg.cc/jqpPH28m/asdaw.jpg","https://i.postimg.cc/vH593ykB/asdsa.jpg","https://i.postimg.cc/jSKf19Yh/cvcx.jpg","https://i.postimg.cc/TPNWp3Lk/qwfsa.jpg","https://i.postimg.cc/TPNWp3Lk/qwfsa.jpg","https://i.postimg.cc/vZznwzQw/sacams.jpg","https://i.postimg.cc/gJwLRb3G/sadsaj.jpg","https://i.postimg.cc/QMdT8Tt9/wcsa23.jpg","https://i.postimg.cc/bJKD9VRk/wdcsa.jpg","https://i.postimg.cc/qqytbcmY/wdsa.jpg","https://i.postimg.cc/pVFnLx2Z/wqcsa.jpg","https://i.postimg.cc/yYC3J5xz/wqdsa.jpg","https://i.postimg.cc/MK0M1Dff/wqfsa.jpg","https://i.postimg.cc/sgVBmh56/wqsa.jpg","https://i.postimg.cc/cLXvbLms/wqvcsa.jpg"];
 
@@ -10,7 +14,13 @@ module.exports.run = async (bot, message, args) => {
         .setTitle("Fotoğrafın! <a:heart:553341691822866442>")
         .setColor("RED")
         .setFooter(`Cool Fotoğraf!`, message.author.avatarURL)
+        .setFooter(`RidenLive Tarafından Editlenmiştir.`, message.author.avatarURL)
         .setImage(replies[result]);
+  
+    const embed = new Discord.RichEmbed()
+  .addField(' Bu Komut RidenLive Tarafından Editlenmiştir.')
+  
+  
 
     message.channel.send(gifembed);
 };
--- komutlar/elraenn.js
+++ komutlar/elraenn.js
@@ -1,6 +1,8 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
+  
+
 
     let replies = ["https://i.postimg.cc/Ls4HRwK3/12312.jpg","https://i.postimg.cc/1tfsVFzg/12321.jpg","https://i.postimg.cc/KjTbJyBx/213211.jpg","https://i.postimg.cc/bJW8xvGm/89123.jpg","https://i.postimg.cc/sDpyWs3J/cnquwq.jpg","https://i.postimg.cc/g2mpqQJb/cnuwq.jpg","https://i.postimg.cc/6pcXHc22/feokwo.jpg","https://i.postimg.cc/gcLGY35Q/qc2.jpg","https://i.postimg.cc/TY8fhpvw/quwh.jpg","https://i.postimg.cc/s22RbW7n/qwec.jpg","https://i.postimg.cc/43FTG31X/qwewq.jpg","https://i.postimg.cc/Ss2bxpZC/qwewqcx.jpg","https://i.postimg.cc/LXcKq077/qwewqeq.jpg","https://i.postimg.cc/Y0QcQzBS/sadaw.jpg","https://i.postimg.cc/05SqgPKq/ss.jpg","https://i.postimg.cc/Dz0txT3H/tugkan-gonultas-elraenn-kimdir.jpg","https://i.postimg.cc/FRJwQVRx/vew.jpg","https://i.postimg.cc/SxDBLK3c/wq.jpg","https://i.postimg.cc/FHrtJSW9/wqeq.jpg","https://i.postimg.cc/MKb2T5YP/wqeqc.jpg","https://i.postimg.cc/1zdbTj9t/wqewq.jpg","https://i.postimg.cc/2yRMjLLN/wqewqec.jpg"];
 
@@ -10,7 +12,11 @@ module.exports.run = async (bot, message, args) => {
         .setTitle("Fotoğrafın! <a:heart:553341691822866442>")
         .setColor("RED")
         .setFooter(`Elraenn Fotoğraf!`, message.author.avatarURL)
+        .setFooter(`RidenLive Tarafından Editlenmiştir.`, message.author.avatarURL)
         .setImage(replies[result]);
+  
+    const embed = new Discord.RichEmbed()
+  .addField(' Bu Komut RidenLive Tarafından Editlenmiştir.')
 
     message.channel.send(gifembed);
 };
