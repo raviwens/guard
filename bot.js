49d02e4b25ae759a6233188087a6369007882bdd
Glitch (hello-express) <none>
2019-07-07 19:09:58 +0000

    📼🍟 Checkpoint ./bot.js:4799852/398 ./watch.json:4799852/2

--- bot.js
+++ bot.js
@@ -1033,7 +1033,17 @@ client.on('message', async message => {
 });
 
 
-
+client.on("message", message => {
+    const dmchannel = client.channels.find("name", "dm-log");
+    if (message.channel.type === "dm") {
+        if (message.author.bot) return;
+        dmchannel.sendMessage("", {embed: {
+            color: 3447003,
+            title: `Gönderen: ${message.author.tag}`,
+            description: `Bota Özelden Gönderilen DM: ${message.content}`
+        }})
+    }
+});
 
 
 client.login(ayarlar.token);
